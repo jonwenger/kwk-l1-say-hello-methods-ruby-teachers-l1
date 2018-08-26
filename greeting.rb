b038c014b7959651d6bdab25e327a7f18c733624
Jon Wenger <jonwenger783@gmail.com>
2018-08-26 16:15:08 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting(local_greeting, your_name, my_name, time_of_day)
+  
+  puts "#{local_greeting} #{your_name}, I am #{my_name}, how's yours #{time_of_day}?"
+  
+end
+
+greeting("What's up", "Kim", "Kanye", "morning")
---
+++ wake_up.rb
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def wake_up
+puts "hear alarm and say UUUUHHHHHH"
+15.times do 
+  puts "snooze alarm"
+end
+puts "fine, I guess I'll wake up"
+
+puts "then go!"
+end
+
+wake_up
